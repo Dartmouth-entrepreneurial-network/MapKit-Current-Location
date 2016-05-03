fe665e2435b6326e8441172dbe8c162d21fa38f4
Sam Putnam <samhputnam@gmail.com>
2016-05-03 10:47:25 -0400

    Mapkit Methods

--- FollowMe/ViewController.swift
+++ FollowMe/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Set up location manager
+        self.locationManager.delegate = self
+        
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // Only want location services to be used when using the app
+        self.locationManager.requestWhenInUseAuthorization()
+        // Turns on location manager
+        self.locationManager.startUpdatingLocation()
+        // Show the blue dot
+        self.mapView.showsUserLocation = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+        // MARK - Location Delegate Methods
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // Get last location of locations that have been passed in
+        let location = locations.last
+        // Get center of that location, i.e. latitude and longitude from that location variable
+        let center =  CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        //  Area for map to zoom to. A smaller number zooms further in. Zooming into user's current location.
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta:1, longitudeDelta: 1))
+    
+        self.mapView.setRegion(region, animated: true)
+        //Now that user's current location is known the location can be stopped from updating.
+        self.locationManager.stopUpdatingLocation()
+    }
+    //handles case if location manager has an error
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print ("Errors: "  + error.localizedDescription)
+    }
+    
 }
 
