07448047d9fcba354fac4557af41caa664483a9a
Sam Putnam <samhputnam@gmail.com>
2016-05-06 08:28:47 -0400

    IB toolsVersion, same mapView, y canvasLocation, print location, building out comments

--- FollowMe/ViewController.swift
+++ FollowMe/ViewController.swift
@@ -13,6 +13,7 @@ import CoreLocation
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+
     
     let locationManager = CLLocationManager()
     
@@ -20,7 +21,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         super.viewDidLoad()
         // Set up location manager
         self.locationManager.delegate = self
-        
+        //Set accuracy to be the best
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
         // Only want location services to be used when using the app
         self.locationManager.requestWhenInUseAuthorization()
@@ -40,13 +41,15 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         // Get last location of locations that have been passed in
         let location = locations.last
+        // For future use
+        print(location)
         // Get center of that location, i.e. latitude and longitude from that location variable
         let center =  CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
-        //  Area for map to zoom to. A smaller number zooms further in. Zooming into user's current location.
+        //  Area for map to zoom to. A smaller number zooms further in. Zooming into user's current location. MKCoordinateRegion is a structure which defines which portion of the map to display; center is the centerpoint of the region displayed. The span defines the horizontal and vertical span representing the amount of map to display. The span also defines the current zoom level used by the map view object.
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta:1, longitudeDelta: 1))
-    
+        // change currently visible region and animate this change
         self.mapView.setRegion(region, animated: true)
-        //Now that user's current location is known the location can be stopped from updating.
+        //Stop the generation of location updates.
         self.locationManager.stopUpdatingLocation()
     }
     //handles case if location manager has an error
